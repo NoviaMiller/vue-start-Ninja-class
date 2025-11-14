c2534a524e17e155952db14626072434824465f6
Novia_Miller <noviamiller105@gmail.com>
2025-11-14 14:10:07 +0800

    Add files via upload
    lec6: create a time reaction

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>time-reaction</h1>
+  <button @click = "startPlay" :disabled="isPlay">PLAY</button>
+  <Block v-if = "isPlay" :delay = "delay" @end = "endGame"/>
+  <Results v-if = "showScore" :score = 'score'/>
+</template>
+
+<script>
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
+
+export default {
+  name: 'App',
+  components: {Block, Results},
+  data(){
+    return {
+      isPlay: false,
+      delay: null,
+      score: null,
+      showScore: false
+    }
+  },
+  methods:{
+    startPlay(){
+      this.isPlay = !this.isPlay;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showScore = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.showScore = true
+      this.isPlay = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
